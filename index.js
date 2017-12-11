bfb32b810e96a391752524bb473e3ed9fbc2e3be
OstlerDev <OstlerDev@users.noreply.github.com>
2017-12-11 10:51:37 -0800

    Swap out CORS

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Import express as webserver connection
 var express = require('express');
-// Import cors for express
-var cors = require('cors')
 // Import other express assets needed to parse JSON from POST's
 var bodyParser = require('body-parser');
 var multer = require('multer');
@@ -49,19 +47,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 // CORS stuff
-var whitelist = ['*']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  }else{
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
-
-app.use(cors(corsOptionsDelegate));
-app.options('*', cors(corsOptionsDelegate)) // include before other routes
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Listen to connections on "http://faucet:port/"
 // Respond with a status of being online.

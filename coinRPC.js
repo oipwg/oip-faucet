f730179f6b3d9c3789d05c3808ba8752c435387b
Sky Young <sky@alexandria.io>
2018-10-04 13:56:54 -0600

    500ms delay instead of 100ms

--- coinRPC.js
+++ coinRPC.js
@@ -19,7 +19,7 @@ function sendToAddress (coin, address, amount, onSuccess, onError){
 		} else {
 			try {
 				// Wait a bit...
-				await async function (){ let res = Promise.resolve; setTimeout(function(){res()}, 100) }
+				await async function (){ let res = Promise.resolve; setTimeout(function(){res()}, 500) }
 
 				client.getRawTransaction(result, 1, function(err, res){
 					if (err){
